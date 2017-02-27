adc9faac8b4546f88ed580f0c6fdb73c330652ce
guillemolero <guillemolerogarcia@gmail.com>
2017-02-27 17:59:05 +0100

    Tarea finalizada
    fin de la tarea

--- include/form.php
+++ include/form.php
@@ -6,12 +6,18 @@
  * Time: 14:45
  */
 ?>
+<div id="mensaje">
 
-<form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
-    <input type="text" name="user" placeholder="Usuario">
-    <a href="#"><p>Comprobar disponibilidad</p></a>
-    <input type="password" name="password" placeholder="Contraseña">
-    <input type="password" name="password2" placeholder="Repite contraseña">
-    <input type="email" name="email" placeholder="Correo electrónico">
-    <input type="submit" name="enviar" value="Enviar">
-</form>
+</div>
+<br>
+<div id="capaformulario">
+    <form id="formulario">
+        <input type="text" name="user" placeholder="Usuario">
+        <a href="#"><p>Comprobar disponibilidad</p></a>
+        <input type="password" name="password" placeholder="Contraseña">
+        <input type="password" name="password2" placeholder="Repite contraseña">
+        <input type="email" name="email" placeholder="Correo electrónico">
+        <input type="button" value="Enviar" onclick="xajax_procesar_formulario(xajax.getFormValues('formulario'))">
+    </form>
+</div>
+
--- index.php
+++ index.php
@@ -6,12 +6,68 @@
  * Time: 14:41
  */
 
+require ("xajax/xajax.inc.php");
+
+$xajax = new xajax();
+
+function procesar_formulario($form_entrada){
+    //Creo el xajaxResponse para generar una salida
+    $respuesta = new xajaxResponse('ISO-8859-1');
+
+    //validacion
+    $error_form = "";
+    if($form_entrada["user"] == ""){
+        $error_form = "Debes escribir tu usuario";
+    }
+    elseif($form_entrada["password"] == ""){
+        $error_form = "Debes escribir tu contraseña";
+    }
+    elseif($form_entrada["password2"] == ""){
+        $error_form = "Debes repetir la contraseña";
+    }
+    elseif($form_entrada["password"] != $form_entrada['password2']){
+        $error_form = "Las contraseñas deben coincidir";
+    }
+    elseif($form_entrada["email"] == ""){
+        $error_form = "Debes escribir tu email";
+    }
+
+    //compruebo resultado de la validacion
+    if($error_form != ""){
+        //Hubo un error en el formulario
+        //en la capa donde se muestran mensajes, muestro el error
+        $respuesta->addAssign("mensaje","innerHTML","<span style='color:red;'>$error_form</span>");
+    }
+    else{
+        //es que no hubo error en el formulario
+        $salida = "Hemos procesado esto:<p>";
+        $salida .= "Nombre: " . $form_entrada["nombre"];
+        $salida .= "<br>Apellidos: " . $form_entrada["apellidos"];
+
+        //mostramos en capa mensaje el texto que está todo correcto
+        $respuesta->addAssign("mensaje","innerHTML","<span style='color:blue;'>Todo correcto... Muchas gracias!</span>");
+        //escribimos en la capa con id="capaformulario el texto que aparece en $salida
+        $respuesta->addAssign("capaformulario","innerHTML",$salida);
+
+        //tenemos que devolver la instanciación del objeto xajaxResponse
+    }
+
+    return $respuesta;
+}
+
+//Asociamos la funcion creada antoriormente al objeto xajax
+$xajax->registerFunction("procesar_formulario");
+//El objeto xajax tiene que procesar cualquier peticion
+$xajax->processRequests();
 ?>
+
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>a</title>
+    <title>XAJAX Validación</title>
+    <?php $xajax->printJavascript("xajax/"); ?>
     <style>
         input {
             display: block;
